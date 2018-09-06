b76d78bd90c24fdb370eec379494cc33d7ca3b85
Ryan Davis <ryan.davis@bison.howard.edu>
2018-09-06 14:11:52 -0400

    added stuff

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Ryan Davis on 9/6/18.
+//  Copyright © 2018 Ryan Davis. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
